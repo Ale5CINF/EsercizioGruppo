5216a5973425c887bd7da7fc7d4fb2e167b5b342
lucag <lucagallo2002.lg@gmail.com>
2021-11-30 11:35:01 +0100

    Aggiungo il toast gallo

--- app/src/main/java/com/zproale/tpsitnuovoprogetto/MainActivity.java
+++ app/src/main/java/com/zproale/tpsitnuovoprogetto/MainActivity.java
@@ -2,6 +2,10 @@ package com.zproale.tpsitnuovoprogetto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+
+import android.widget.Toast;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Gallo";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
     }
 }
